ae2604d96da0bd1eaf51968cf821d5c661bd912d
rnadkarni2 <58921102+rnadkarni2@users.noreply.github.com>
2021-08-04 17:06:20 -0700

    Removed commented out code and white space

--- DCE_IDandPhaseSelect/Breast_DCEMRI_FTV_plugins1/Exam_Ident_and_timing.py
+++ DCE_IDandPhaseSelect/Breast_DCEMRI_FTV_plugins1/Exam_Ident_and_timing.py
@@ -65,60 +65,6 @@ def runExamIdentAndTiming(exampath,dce_folders_manual,dce_ind_manual,earlyadd,la
         if(len(dce_folders_manual) == 0 and len(dce_folders) == 0):
             slicer.util.confirmOkCancelDisplay("Automatic series ID failed. Please try manual series ID for this exam","Series ID Failed")
 
-
-
-
-        #Edit 11/25/2020: commenting out 11/24 edit because I noticed that temporal resolution is on the order of ~95 seconds
-        #Edit 11/24/2020: Add fix to change from 2 folders to 1 if gap between 1st and 2nd phases of multivolume folder is > 10 seconds
-##        if(len(dce_folders) == 2):
-##            if('gunzipped' in exampath):
-##                orig_exampath = exampath[:-10] #remove \gunzipped from the end to get original exampath
-##                gzip_gunzip_pyfuncs.gunzipAllFilesDCE(orig_exampath,str(dce_folders[0]))
-##                gzip_gunzip_pyfuncs.gunzipAllFilesDCE(orig_exampath,str(dce_folders[1]))
-##
-##            if(folder1info.totslices/folder1info.im_in_acq > 1):
-##                multiinfo = folder1info
-##                multifolder = img_folders[dce_ind[0]]
-##                multiind = dce_ind[0]
-##            else:
-##                folder2info = all_folders_info[1]
-##                if(folder2info.totslices/folder2info.im_in_acq > 1):
-##                    multiinfo = folder2info
-##                    multifolder = img_folders[dce_ind[1]]
-##                    multiind = dce_ind[1]
-##
-##            multipath = os.path.join(exampath,str(multifolder))
-##            multifiles = os.listdir(multipath)
-##            multictimes = []
-##            multitrigtimes = []
-##            for mf in range(len(multifiles)):
-##                curr_file = multifiles[mf]
-##                curr_path = os.path.join(multipath,curr_file)
-##                try:
-##                    curr_hdr = pydicom.dcmread(curr_path,stop_before_pixels = True)
-##                except:
-##                    curr_hdr = dicom.read_file(curr_path)
-##                curr_ctime = curr_hdr.ContentTime
-##                curr_ctimesec = 3600*int(curr_ctime[0:2]) + 60*int(curr_ctime[2:4]) + float(curr_ctime[4:])
-##                multictimes.append(curr_ctimesec)
-##                multitrigtimes.append(curr_hdr.TriggerTime/1000) #convert to seconds
-##
-##            multictimes = np.array(multictimes)
-##            multictimes = np.unique(multictimes)
-##
-##            #For some exams, like UKCC, use trigger time in place of content time
-##            if(len(multictimes) == 1):
-##                multitrigtimes = np.array(multitrigtimes)
-##                multictimes = np.unique(multitrigtimes)
-##
-##            p1p2diff = float(multictimes[1]) - float(multictimes[0])
-##            if(p1p2diff > 10):
-##                print("There is pause of > 10 sec between phases 1 and 2 of multivolume folder. Therefore, this is the only DCE folder.")
-##                dce_folders = [multifolder]
-##                dce_ind = [multiind]
-        #Edit 11/24/2020 ends here
-
-
         #folder1info = all_folders_info[0]
         manufacturer = folder1info.manufacturer
         try:
@@ -145,8 +91,7 @@ def runExamIdentAndTiming(exampath,dce_folders_manual,dce_ind_manual,earlyadd,la
             print( "Early post-contrast image is post-contrast #" + str(earlyPostContrastNum) )
             print( "Late post-contrast image is post-contrast #" + str(latePostContrastNum) )
 
-
-
+            
     if ('SIEMENS' in folder1info.manufacturer or 'Siemens' in folder1info.manufacturer):
         print("---------- Siemens exam identification ----------")
         #Edit 2/12/2021: Only run DCE folder ident if dce_folders_manual (list of user's manual DCE folder selections) is empty
@@ -181,7 +126,7 @@ def runExamIdentAndTiming(exampath,dce_folders_manual,dce_ind_manual,earlyadd,la
             print( "Early post-contrast image is post-contrast #" + str(earlyPostContrastNum) )
             print( "Late post-contrast image is post-contrast #" + str(latePostContrastNum) )
 
-
+            
     if ('PHILIPS' in folder1info.manufacturer or 'Philips' in folder1info.manufacturer):
         print("---------- Philips exam identification ----------")
         #Edit 2/12/2021: Only run DCE folder ident if dce_folders_manual (list of user's manual DCE folder selections) is empty
